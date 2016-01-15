f98f9400569f87125264a7a59a8717614dcae973
yacsap <yacsap@gmail.com>
2016-01-15 17:16:14 +1300

    Create RepliCat.bat
    First Release of RepliCat.bat

---
+++ RepliCat.bat
@@ -0,0 +1,29 @@
+:: TransactorTech: RepliCat v1.0
+:: Created: 2016-01-15 | Andy Saputra
+:: E: andy.saputra@transactortech.com
+:: ============================================================
+:: -> 1-way folder syncronization script (pull method)
+:: -> run this from DR site
+:: ============================================================
+:: Turning off the technical commands from the screen..
+@echo off
+::
+echo Detaching any previous drive connected to T:
+net use T: /D /YES
+:: ============================================================
+:: Make sure TCP+UDP 135-139 & 445 is opened on the primary !!
+:: ============================================================
+echo Connecting to GTW-Tomcat T: (Tomcat drive)
+net use T: \\1.2.3.4\c$ /user:login password
+::
+echo Switching to T: (Remote / Master Tomcat drive)
+T:
+::
+echo Changing directory to Tomcat
+cd T:\src
+::
+echo Replicating Tomcat to DR Server (this server)
+robocopy T:\src\ C:\dst /Z /E
+::
+exit
+:: End of this batch file :)
