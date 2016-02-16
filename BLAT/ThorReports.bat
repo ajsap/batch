0b31252826490ee1ac119112b7c67def9c210f6f
yacsap <yacsap@gmail.com>
2016-02-16 16:08:06 +1300

    Update ThorReports.bat

--- BLAT/ThorReports.bat
+++ BLAT/ThorReports.bat
@@ -14,7 +14,7 @@
 ECHO Sending Thor Reports
 if exist D:\thorfiles\REP_*.txt CD /D D:\thorfiles 
 if exist D:\thorfiles\REP_*.txt REN REP_*.txt REP_*.csv
-::if exist D:\thorfiles\REP_*.csv C:\BLAT\BLAT.EXE -body "Reports attached." -server 1.2.3.4 -u administration@transactortech.com -pw passwd -f donotreply@transactortech.com -subject "Farmers Reports" -try 30 -to Sherilyn.Jonkers@farmers.co.nz -bcc andy.saputra@transactortech.com,reports@transactortech.com -attach D:\thorfiles\REP_*.csv
+::if exist D:\thorfiles\REP_*.csv C:\BLAT\BLAT.EXE -body "Reports attached." -server 1.2.3.4 -u administration@transactortech.com -pw passwd -f donotreply@transactortech.com -subject "Farmers Reports" -try 30 -to manager@company.tld -bcc andy.saputra@transactortech.com,reports@transactortech.com -attach D:\thorfiles\REP_*.csv
 :: FOR TESTING, DISABLE MOVE PROCEDURE BELOW AND USE ::>
 C:\BLAT\BLAT.EXE -body "[TEST] Reports attached." -server 1.2.3.4 -u administration@transactortech.com -pw passwd -f donotreply@transactortech.com -subject "Farmers Reports" -try 30 -to ursula.main@transactortech.com -bcc andy.saputra@transactortech.com,reports@transactortech.com -attach D:\thorfiles\REP_*.csv
 
