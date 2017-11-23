4b0f3e01af41344d068a062b4c541140fb4365ea
yacsap <yacsap@gmail.com>
2017-11-23 15:51:15 +1300

    Create wbst.bat

---
+++ WebsiteBandwidthStressTest/wbst.bat
@@ -0,0 +1,12 @@
+:: Website Bandwidth Stress Test v1.0-BAT
+:: Created: 2017-11-23 | Andy Saputra
+:: https://saputra.ch
+@echo off
+set /a count=0
+:start
+cls
+echo File has been downloaded %count% times!
+echo.
+wget -O NUL http://cachefly.cachefly.net/100mb.test
+set /a count+=1
+goto start
