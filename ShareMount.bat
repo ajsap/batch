654696aea12c5df37cdb8c49e899b32d05647e6d
ajsap <yacsap@gmail.com>
2020-01-29 11:58:57 +1300

    Update ShareMount.bat
    Updated mount messages

--- ShareMount.bat
+++ ShareMount.bat
@@ -1,18 +1,18 @@
-::::::::::::::::::::::::::::::::::::::::
-:: Simple Windows Share Mounter v1.0  ::
-:: Can be scheduled to run on boot :) ::
-:: Created: 2016-01-26 | andy@indo.fm ::
-::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::
+:: Simple Windows Share Mounter v1.1      ::
+:: Can be scheduled to run on boot :)     ::
+:: Created: 2016-01-26 | andy@saputra.org ::
+::::::::::::::::::::::::::::::::::::::::::::
 
 @ECHO OFF
-ECHO Detaching any previous drive connected to S:
+ECHO Detaching all previously attached network shares..
 NET USE S: /D /YES
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Make sure TCP+UDP 135-139 & 445 is opened on remote side. ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-ECHO Connecting to Samba S: drive..
+ECHO Attaching network shares..
 NET USE S: \\ip-or-address\share /user:Username Password
 
 EXIT
